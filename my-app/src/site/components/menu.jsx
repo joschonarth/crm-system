f3aebaa0d899e085f25fb2d57fa80a6e02760d7d
joschonarth <joschonarth@gmail.com>
2024-07-27 10:26:21 -0300

    Update

--- my-app/src/site/components/menu.jsx
+++ my-app/src/site/components/menu.jsx
@@ -6,7 +6,7 @@ function Menu(){
     <div className="container">
         {/* <a href="#" className="navbar-brand">99 CRM</a> */}
         <a href="#" class="navbar-brand">
-                <img src="Images/logo.png" alt="" height="28"/>
+                <img src="images/logo.png" alt="" height="28"/>
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
