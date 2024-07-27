87a06535b840882555ecd92262b98f8c8e92b902
joschonarth <joschonarth@gmail.com>
2024-07-27 10:44:03 -0300

    Update

--- my-app/src/site/site.jsx
+++ my-app/src/site/site.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Menu from './components/menu'
-import Banner from './components/banner'
+import Menu from './components/menu';
+import Banner from './components/banner';
+import Features from './components/features';
 
 function Site(){
     return <div>
         <Menu/>
         <Banner/>
+        <Features/>
     </div>;
 }
 
