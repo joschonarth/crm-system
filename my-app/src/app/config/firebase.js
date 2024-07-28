bb18ea6599dde4b2e03af90b3b9da91217b4e94c
joschonarth <joschonarth@gmail.com>
2024-07-28 11:57:59 -0300

    Update CRM System

--- my-app/src/app/config/firebase.js
+++ my-app/src/app/config/firebase.js
@@ -1,7 +1,7 @@
-import firebase from 'firebase';
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +16,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
 
-export default app;
+export { firebase, auth };
+// export default firebase;
--- my-app/src/app/login/login.jsx
+++ my-app/src/app/login/login.jsx
@@ -1,32 +1,65 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { auth } from '../config/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import './login.css';
 
-function Login(){
-    return <div className='d-flex align-items-center text-center form-container'>
-    <form className='form-signin'>
-        <img className="mb-4" src="img/logo-small2.png" alt=""/>
-        <h1 className="h3 mb-3 fw-normal">Login</h1>
+function Login() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [sucesso, setSucesso] = useState('');
 
-        <div className="form-floating">
-            <input type="email" className="form-control" id="floatingInput" placeholder="Email"/>
-            <label for="floatingInput">Email</label>
+    function loginUsuario() {
+        signInWithEmailAndPassword(auth, email, senha)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                // alert('Login realizado com sucesso!');
+                setSucesso('Y');
+            })
+            .catch((error) => {
+                // const errorCode = error.code;
+                // const errorMessage = error.message;
+                // alert(errorMessage);
+                setSucesso('N');
+            });
+    }
+
+    function alterarEmail(event) {
+        setEmail(event.target.value);
+    }
+
+    function alterarSenha(event) {
+        setSenha(event.target.value);
+    }
+
+    return (
+        <div className='d-flex align-items-center text-center form-container'>
+            <form className='form-signin'>
+                <img className="mb-4" src="img/logo-small2.png" alt=""/>
+                <h1 className="h3 mb-3 fw-normal">Login</h1>
+
+                <div className="form-floating">
+                    <input onChange={alterarEmail} type="email" className="form-control" id="floatingInput" placeholder="Email"/>
+                    <label htmlFor="floatingInput">Email</label>
+                </div>
+                <div className="form-floating">
+                    <input onChange={alterarSenha} type="password" className="form-control" id="floatingPassword" placeholder="Senha"/>
+                    <label htmlFor="floatingPassword">Senha</label>
+                </div>
+
+                <button onClick={loginUsuario} className="btn btn-primary w-100 py-2" type="button">Acessar</button>
+
+                { sucesso === 'N' ? <div className="alert alert-danger mt-2" role="alert">Email ou Senha inválidos!</div> : null }
+
+                <div className='login-links mt-5'>
+                    <Link to="/app/reset-senha" className='mx-3'>Esqueci minha senha</Link>
+                    <Link to="/app/cadastro" className='mx-3'>Criar uma conta</Link>
+                </div>
+
+                <p className="mt-5 mb-3 text-body-secondary">&copy; Desenvolvido por 99 Coders</p>
+            </form>
         </div>
-        <div className="form-floating">
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Senha"/>
-            <label for="floatingPassword">Senha</label>
-        </div>
-
-        <button className="btn btn-primary w-100 py-2" type="submit">Acessar</button>
-
-        <div className='login-links mt-5'>
-            <Link to="/app/reset-senha" className='mx-3'>Esqueci minha senha</Link>
-            <Link to="/app/cadastro" className='mx-3'>Criar uma conta</Link>
-        </div>
-
-        <p className="mt-5 mb-3 text-body-secondary">&copy; Desenvolvido por 99 Coders</p>
-  </form>
-</div>;
+    );
 }
 
 export default Login;
