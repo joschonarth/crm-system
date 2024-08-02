e1230c0a896a05f4ac82dc38d291a5145a8dd841
joschonarth <joschonarth@gmail.com>
2024-08-02 19:53:46 -0300

    Update CRM System

--- my-app/src/app/reports/clientes/clientes.jsx
+++ my-app/src/app/reports/clientes/clientes.jsx
@@ -4,18 +4,50 @@ import pdfFonts from 'pdfmake/build/vfs_fonts';
 function clientesPDF(clientes){
     pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
-    const reportTitle = [];
+    const reportTitle = [
+        {
+            text: 'Clientes',
+            fontSize: 15,
+            bold: true,
+            margin: [15, 20, 0, 45] // left, top, right, bottom
+        }
+    ];
 
-    const details = [];
+    const details = [
+        {
+            table: {
+                headerRows: 1,
+                widths: ['*', '*', '*', '*'],
+                body: [
+                    [
+                        {text: 'Código', style: 'tableHeader', fontSize: 10},
+                        {text: 'Nome', style: 'tableHeader', fontSize: 10},
+                        {text: 'Email', style: 'tableHeader', fontSize: 10},
+                        {text: 'Telefone', style: 'tableHeader', fontSize: 10},
+                    ],
+                ]
+            },
+            layout: 'headerLineOnly'
+        }
+    ];
 
-    const footer = [];
+    function Footer(currentPage, pageCount){
+        return [
+            {
+                text: currentPage + ' / ' + pageCount,
+                alignment: 'right',
+                fontSize: 10,
+                margin: [0, 10, 20, 0] // left, top, right, bottom
+            }
+        ]
+    }
 
     const docDefinitions = {
         pageSize: 'A4',
         pageMargins: [15, 50, 15, 40],
         header: [reportTitle],
         content: [details],
-        footer: [footer]
+        footer: Footer
     }
 
     pdfMake.createPdf(docDefinitions).download();
