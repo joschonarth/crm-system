3cd921b25be4888f238c3e2388cf629eabd869c8
joschonarth <joschonarth@gmail.com>
2024-08-02 23:36:49 -0300

    Update CRM System

--- my-app/src/app/config/firebase.js
+++ my-app/src/app/config/firebase.js
@@ -3,11 +3,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
+  /* Insira as credenciais do seu projeto do Firebase */
   apiKey: "AIzaSyC_z9PsPWqH3cO27O-U2b56PxEzZIDXbOI",
   authDomain: "crm-system-157b8.firebaseapp.com",
   projectId: "crm-system-157b8",
